e56291323464b0d1bdb5311aa807b4251dce053f
Eultype <Sdarryy59@gmail.com>
2026-01-31 17:53:24 +0100

    feat(ui): amélioration du footer (responsive + ajout de la font Cormorant)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
     return (
         <footer className="bg-black text-white py-16">
             <div className="container mx-auto px-4">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 mb-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-12 mb-16">
                     {/* Colonne 1 : Logo  / Description */}
                     <div className="space-y-6">
                         {/* Logo */}
